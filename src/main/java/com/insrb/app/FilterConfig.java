bd93f0829187327d3739837db9b0ea55c73704bf
insurobo.dev <insurobo.dev@gmail.com>
2021-09-07 14:14:20 +0900

    20210907_COMMIT

--- src/main/java/com/insrb/app/FilterConfig.java
+++ src/main/java/com/insrb/app/FilterConfig.java
@@ -1,18 +1,12 @@
 package com.insrb.app;
 
 import com.insrb.app.filter.AuthFilter;
-import org.apache.catalina.connector.Connector;
-import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class FilterConfig implements WebMvcConfigurer {
+public class FilterConfig {
 
 	// uncomment this and comment the @Component in the filter class definition to register only for a url pattern
 	@Bean
@@ -27,24 +21,5 @@ public class FilterConfig implements WebMvcConfigurer {
 				"/brs/*", "/hsart/*", "/ocr/*", "/apis/*", "/sociallogin/*");
 
 		return registrationBean;
-	}
-
-	@Bean
-	public ConfigurableServletWebServerFactory webServerFactory() {
-		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-		factory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
-			@Override
-			public void customize(Connector connector) {
-				connector.setProperty("relaxedQueryChars", "|{}[]");
-			}
-		});
-		return factory;
-	}
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-				.allowedOrigins("*")
-				.allowedMethods("GET", "POST");
 	}
 }
